a80db36ab0cdca6e0d7400fceeef2ece9fbe3667
Yuna Iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-05-03 15:07:05 +0100

    moved to tab navigator component

--- App.tsx
+++ App.tsx
@@ -1,67 +1,14 @@
-import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import CreatePixelArt from './views/CreatePixelArtStack/CreatePixelArt';
-import HomeScreen from './views/HomeScreen';
-import ProfileScreen from './views/ProfileScreen';
-import Feather from 'react-native-vector-icons/Feather';
-import Octicons from 'react-native-vector-icons/Octicons';
 
-const Tab = createBottomTabNavigator();
+import TabNavigator from './navigation/TabNavigator';
 
 function App() {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              headerStyle: {
-                backgroundColor: 'black',
-              },
-              headerTintColor: 'white',
-              tabBarShowLabel: false,
-              tabBarStyle: {backgroundColor: 'black'},
-              tabBarIcon: () => (
-                <Feather name="home" style={{color: 'white', fontSize: 30}} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Create"
-            component={CreatePixelArt}
-            options={{
-              headerStyle: {
-                backgroundColor: 'black',
-              },
-              headerTintColor: 'white',
-              tabBarShowLabel: false,
-              tabBarStyle: {backgroundColor: 'black'},
-              tabBarIcon: () => (
-                <Octicons
-                  name="diff-added"
-                  style={{color: 'white', fontSize: 30}}
-                />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              headerStyle: {
-                backgroundColor: 'black',
-              },
-              headerTintColor: 'white',
-              tabBarShowLabel: false,
-              tabBarStyle: {backgroundColor: 'black'},
-            }}
-          />
-        </Tab.Navigator>
+        <TabNavigator />
       </NavigationContainer>
     </GestureHandlerRootView>
   );
--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -3,14 +3,60 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../views/HomeScreen';
 import CreatePixelArt from '../views/CreatePixelArtStack/CreatePixelArt';
+import ProfileScreen from '../views/ProfileScreen';
+import Feather from 'react-native-vector-icons/Feather';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Create" component={CreatePixelArt} />
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+          tabBarShowLabel: false,
+          tabBarStyle: {backgroundColor: 'black'},
+          tabBarIcon: () => (
+            <Feather name="home" style={{color: 'white', fontSize: 30}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={CreatePixelArt}
+        options={{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+          tabBarShowLabel: false,
+          tabBarStyle: {backgroundColor: 'black'},
+          tabBarIcon: () => (
+            <Octicons
+              name="diff-added"
+              style={{color: 'white', fontSize: 30}}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+          tabBarShowLabel: false,
+          tabBarStyle: {backgroundColor: 'black'},
+        }}
+      />
     </Tab.Navigator>
   );
 }
