9e22372cd75347e6452b7a2d84e3d2d05d33f1d3
Yuna Iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-05-15 12:42:27 +0100

    initial grid passed through

--- src/components/DrawingPanelTwo.tsx
+++ src/components/DrawingPanelTwo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {View, TouchableWithoutFeedback} from 'react-native';
+import {View, TouchableWithoutFeedback, Dimensions} from 'react-native';
+import {create} from 'react-test-renderer';
 
-const Pixel = () => {
+const Pixel = ({width, pixelColour}) => {
   return (
-    <TouchableWithoutFeedback delayPressOut={0} onPress={applyColor}>
+    <TouchableWithoutFeedback delayPressOut={0}>
       <View
         style={{
           width: width,
-          height: height,
+          height: width,
           borderWidth: 0.25,
           borderColor: '#404040',
           backgroundColor: pixelColour,
@@ -20,7 +21,7 @@ const generateEmptyGrid = (length: number) => {
   const emptyArray = Array.from({length}, (_, i) => []);
   emptyArray.forEach(item => {
     for (let i = 0; i < length; i++) {
-      item.push({selectedColour: 'black'});
+      item.push({pixelColour: 'pink'});
     }
   });
 
@@ -29,12 +30,39 @@ const generateEmptyGrid = (length: number) => {
 
 export default function DrawingPanelTwo({gridSize}) {
   const pixelGrid = generateEmptyGrid(gridSize);
+
+  const createPixelGrid = () => {
+    const rows = [];
+    const gridWidth = Dimensions.get('window').width;
+    const pixelWidth = gridWidth / gridSize;
+
+    for (let i = 0; i < gridSize; i++) {
+      const pixels = [];
+      for (let j = 0; j < gridSize; j++) {
+        let pixelColour = pixelGrid[i][j].pixelColour;
+        pixels.push(<Pixel width={pixelWidth} pixelColour={pixelColour} />);
+      }
+      rows.push(
+        <View
+          key={`row-${i}`}
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+          }}>
+          {pixels}
+        </View>,
+      );
+    }
+    return rows;
+  };
   return (
     <View
       style={{
         aspectRatio: 1,
         width: '100%',
         backgroundColor: 'black',
-      }}></View>
+      }}>
+      {createPixelGrid()}
+    </View>
   );
 }
